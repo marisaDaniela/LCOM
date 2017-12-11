8322a72ca0b692c73fd1ec52b3bc537951fcc27c
Marisa Oliveira <up201308594@fe.up.pt>
2017-12-11 16:37:44 +0000

    LET THE GAMES BEGINN

--- project/src/main.c
+++ project/src/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include "vbe.h"
+#include "graphics.h"
 
 int main(int argc, char **argv)
 {
-    printf("Hello, world!\n");
-    return 0;
+	srand(time(NULL));
+	sef_startup();
+
+
+	printf("Hello, world!\n");
+
+	void * vgInit = vg_init(0x114);
+	//unsigned char key;
+
+	if( vgInit == NULL )
+	{
+		vg_exit();
+		printf("test_square::failed vg_init()\n");
+		return -1;
+	}
+
+	drawSquare(20,20,500,600);
+
+	sleep(5);
+
+
+	//TODO: KeyESC pressed to leave
+
+	vg_exit();
+	return 0;
 }
---
+++ project/src/utils.c
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+
+int fileExists(const char* filename) {
+	FILE* file = fopen(filename, "r");
+
+	if(file){
+		fclose(file);
+		return 1;
+	}
+	return 0;
+}
