59402d771782075e64d751ccc616945c4b08d792
xJupiter <xJupiter931@gmail.com>
2017-12-23 03:39:45 +0000

    :D

--- project/src/game.c
+++ project/src/game.c
@@ -3,40 +3,43 @@
 
 int game()
 {
-	int ipc_status, r;
-	message msg;
-	int RUNNING = 1;
-
-	if(lm_init()==NULL){
-		return;
-	}
-
-	char *video_mem;
-	video_mem = vg_init(mode);
-
-	if(video_mem == NULL){
-		return;
-	}
-
-	initDoubleBuffer();
-
 	//subscrever as interrupcoes do timer
 	int irq_timer = timer_subscribe_int();
 
 	//subscrever as interrupcoes do teclado
 	int irq_kbd = kbd_subscribe_int();
 
-
 	//subscrever as interrupcoes do rato
 	int irq_mouse = mouse_subscribe_int( );
+	
+	// inicializar gráficos
+	if(lm_init()==NULL)
+	{
+		return;
+	}
 
+	char *video_mem = vg_init(mode);
+
+	if(video_mem == NULL)
+	{
+		return;
+	}
+	
+	// teste
+	initDoubleBuffer();
 
 	Bitmap* lcom = loadBitmap(path('lcom'));
 	clearBuffer();
 	drawBitmap('lcom', 261, 133, ALIGN_LEFT);
-
+	
 	bufferToVideoMem();
+	
+	return;
 
+	// game loop
+	int ipc_status, r;
+	message msg;
+	int RUNNING = 1;
 	while(RUNNING) {
 		if (driver_receive(ANY, &msg, &ipc_status) != OK) {
 			printf("driver_receive failed with: %d\n",r);
